ec63a5e095a5d7308300cb774d1d794cb8a09f48
Jaren Kawai <jkawai@ucsc.edu>
2023-10-16 15:19:40 -0700

    Main file comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+//Jaren Kawai
+//Rocket Patrol II: Refactored
+//Time to complete assignment:
+//Mods:
+//  New timing method - 5pts
+//  Mouse control - 5pts
+//  Display time remaining - 3pts
+//  4 New explosion sounds - 3 pts
+//  New title screen - 3pts
+//  High score - 1pts
+//Sources used:
 let config = {
     type: Phaser.AUTO,
     width: 640,
